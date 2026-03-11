7c2f3357db3bd5ff8b74958ac28e0a7747ae23be
Aarunya Retheep <aarunyaretheep003@gmail.com>
2026-03-11 21:00:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import internalImage from './images/image-nature.jpg';
+import React, { useState } from "react";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  console.log("React app started");
+  const [isSignup, setIsSignup] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
 
-  const userName = "Aarunya";
+  if (!currentUser) {
+    return isSignup ? (
+      <Signup setIsSignup={setIsSignup} />
+    ) : (
+      <Login setCurrentUser={setCurrentUser} setIsSignup={setIsSignup} />
+    );
+  }
 
   return (
-    <div className="container mt-5">
-      <div className="card text-center p-3">
-
-        <h3 style={{ color: 'purple' }}>
-          Welcome to React Learning, {userName}
-        </h3>
-
-        <img
-          src={internalImage}
-          alt="Internal"
-          className="img-fluid mx-auto my-2"
-          style={{ width: '200px' }}
-        />
-
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-          alt="External"
-          className="img-fluid mx-auto my-2"
-          style={{ width: '200px' }}
-        />
-
-        <p className="text-secondary">
-          This is your first card with images and styles!
-        </p>
-
-      </div>
-    </div>
+    <Dashboard
+      currentUser={currentUser}
+      setCurrentUser={setCurrentUser}
+    />
   );
 }
 
